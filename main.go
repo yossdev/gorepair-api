f5c429008236d5e93ebad0dc124f04092013d4c7
yossdev <zenhanprogram@gmail.com>
2021-09-18 23:16:05 +0700

    add group routes, find, sign-up, and sign-in routes; add standardized response; and add json and form feature

--- main.go
+++ main.go
@@ -15,53 +15,174 @@ import (
 func main() {
 	// Server
 	e := echo.New()
+	v1 := e.Group("v1/api/")
 	
 	// User routes
-	e.GET("/v1/users", GetUser)
-	e.GET("/v1/users/:id", GetUser)
+	v1.GET("users", GetUsers)
+	v1.GET("user/:id", UserDetails)
+	v1.POST("sign-up/user", UserRegister)
+	v1.POST("sign-in/user", UserLogin)
 
 	// Workshop routes
-	e.GET("/v1/workshops", GetWorkshop)
-	e.GET("/v1/workshops/:id", GetWorkshop)
+	v1.GET("workshops", GetWorkshops)
+	v1.GET("workshop/:id", WorkshopDetails)
+	v1.GET("workshops/find", FindWorkshop)
+	v1.POST("sign-up/workshop", WorkshopRegister)
+	v1.POST("sign-in/workshop", WorkshopLogin)
 
-	fmt.Println("Starting REST API web server at http://localhost:8000/")
 	e.Start(":8000")
 }
 
 // GoRepair REST API
+type BaseResponse struct {
+	Code 	int
+	Message string
+	Data 	interface{}
+}
+
 type User struct {
-	Id       int
-	Email    string
-	Password string
-	Address  string
+	Id       int	`json:"id" form:"id"`
+	Email    string	`json:"email" form:"email"`
+	Password string	`json:"password" form:"password"`
+	Address  string	`json:"address" form:"address"`
 }
 
 type Workshop struct {
-	Id       int
-	Email    string
-	Password string
-	Address  string
+	Id       int	`json:"id" form:"id"`
+	Email    string	`json:"email" form:"email"`
+	Password string	`json:"password" form:"password"`
+	Address  string	`json:"address" form:"address"`
 }
 
-func GetUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	user := []User{
-		{id, "axe@gmail.com", "axe123", geolocation(42.36399, -71.05493)},
-		{id, "kunkka@gmail.com", "kunkka123", geolocation(37.4224764, -122.0842499)},
-	}
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"user": user,
+type Login struct {
+	Email    string	`json:"email" form:"email"`
+	Password string	`json:"password" form:"password"`
+}
+
+type SignUp struct {
+	Name 	 string `json:"name" form:"name"`
+	Email    string	`json:"email" form:"email"`
+	Password string	`json:"password" form:"password"`
+}
+
+// hardcoded data
+var user = []User{
+	{1, "axe@gmail.com", "axe123", geolocation(42.36399, -71.05493)},
+	{2, "kunkka@gmail.com", "kunkka123", geolocation(37.4224764, -122.0842499)},
+}
+
+func GetUsers(c echo.Context) error {
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code: http.StatusOK,
+		Message: "Success",
+		Data: user,
 	})
 }
 
-func GetWorkshop(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	workshop := []Workshop{
-		{id, "bengkelaxe@gmail.com", "bengkelaxe123", geolocation(42.36399, -71.05493)},
-		{id, "bengkelkunkka@gmail.com", "bengkelkunkka123", geolocation(37.4224764, -122.0842499)},
+func UserDetails(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id")) 
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, BaseResponse{
+			Code: http.StatusInternalServerError,
+			Message: "Id is not valid",
+		})
 	}
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code: http.StatusOK,
+		Message: "Success",
+		Data: User{Id: id}, // will check to database for now it will be empty
+	})
+}
+
+func UserRegister(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+
+	var user SignUp
+	user.Name = name
+	user.Email = email
+	user.Password = password
+
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code: http.StatusOK,
+		Message: "Success",
+		Data: user,
+	})
+}
+
+func UserLogin(c echo.Context) error {
+	login := Login{}
+	c.Bind(&login)
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code: http.StatusOK,
+		Message: "Success",
+		Data: login,
+	})
+}
+
+// hardcoded data
+var workshop = []Workshop{
+	{1, "bengkelaxe@gmail.com", "bengkelaxe123", geolocation(42.36399, -71.05493)},
+	{2, "bengkelkunkka@gmail.com", "bengkelkunkka123", geolocation(37.4224764, -122.0842499)},
+}
+
+func GetWorkshops(c echo.Context) error {
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code: http.StatusOK,
+		Message: "Success",
+		Data: workshop,
+	})
+}
+
+func WorkshopDetails(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, BaseResponse{
+			Code: http.StatusInternalServerError,
+			Message: "Id is not valid",
+			Data: nil,
+		})
+	}
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code: http.StatusOK,
+		Message: "Success",
+		Data: Workshop{Id: id}, // will check to database for now it will be empty
+	})
+}
+
+func FindWorkshop(c echo.Context) error {
+	match := c.QueryParam("name")
 	return c.JSON(http.StatusOK, map[string]interface{}{
-		"workshop": workshop,
+		"name": match,
+		"result": []string{"jaya bengkel", "honda", "suzuki"}, // hardcoded data for now
+	})
+}
+
+func WorkshopRegister(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+
+	var workshop SignUp
+	workshop.Name = name
+	workshop.Email = email
+	workshop.Password = password
+
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code: http.StatusOK,
+		Message: "Success",
+		Data: workshop,
+	})
+}
+
+func WorkshopLogin(c echo.Context) error {
+	login := Login{}
+	c.Bind(&login)
+	return c.JSON(http.StatusOK, BaseResponse{
+		Code: http.StatusOK,
+		Message: "Success",
+		Data: login,
 	})
 }
 
